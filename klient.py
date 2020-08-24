681fd8ba97f541d2cbb6a580554d1a41efb120d2
unknown <ploscaru.alexandra@yahoo.com>
2020-08-24 21:54:59 +0300

    'nr2'

--- klient.py
+++ klient.py
@@ -1,9 +1,16 @@
 import requests
+class Client:
+    def put(self):
+        url = 'http://127.0.0.1:5000/api/v1/resources/books'
+        params = {"id":"", "title":"Dune","author":"Frank Herbert","first_sentence":"In saptamana dinainte plecarii din Arrakis","published":"1965"}
+        p = requests.post(url, json=params)
+        print(p)
 
-url = 'http://127.0.0.1:5000/api/v1/resources/books'
-params = {"id":"", "title":"Dune","author":"Frank Herbert","first_sentence":"In saptamana dinainte plecarii din Arrakis","published":"1965"}
-p = requests.post(url, json=params)
-print(p)
+    def get(self):
+        url = 'http://127.0.0.1:5000/api/v1/resources/books/all'
+        g=requests.get(url)
+        with open ("myget.txt",'w') as x:
+            x.writelines(str(g.content))
 
-
+Client().get()
 
